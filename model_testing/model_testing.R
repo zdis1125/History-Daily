b1ef74de570b8c0a2d5e4e12859ef8bbb84b7705
zdis1125 <zdis1125@gmail.com>
2026-09-22 12:52:05 -0400

    Update model_testing.R

--- model_testing/model_testing.R
+++ model_testing/model_testing.R
@@ -33,21 +33,22 @@ model_formulaA = score ~ AdjD.A + AdjO.H + AdjT.A + AdjT.H  + AdjO.A + AdjD.H
 model_formulaH = score.1 ~ AdjD.A + AdjO.H + AdjT.A + AdjT.H  + AdjO.A + AdjD.H
 
 model_list <- list(
-  Linear_Regression = function(f, data) lm(f, data = data),
-  Random_Forest     = function(f, data) randomForest(f, data = data, ntree = 500, mtry = 3),
-  Random_Forest2     = function(f, data) randomForest(f, data = data, ntree = 100, mtry = 3),
-  Random_Forest3     = function(f, data) randomForest(f, data = data, ntree = 500, mtry = 2),
-  Random_Forest4     = function(f, data) randomForest(f, data = data, ntree = 100, mtry = 2),
-  #Support_Vector    = function(f, data) svm(f, data = data, cost = 10, gamma = 0.1),
+  #Linear_Regression = function(f, data) lm(f, data = data),
+  #Random_Forest     = function(f, data) randomForest(f, data = data, ntree = 500, mtry = 3),
+  #Random_Forest2     = function(f, data) randomForest(f, data = data, ntree = 100, mtry = 3),
+  #Random_Forest3     = function(f, data) randomForest(f, data = data, ntree = 500, mtry = 2),
+  #Random_Forest4     = function(f, data) randomForest(f, data = data, ntree = 100, mtry = 2),
+  #Support_Vector1    = function(f, data) svm(f, data = data, cost = 10, gamma = 0.1),
+  Support_Vector2    = function(f, data) svm(f, data = data),
   #SVM_Radial        = function(f, data) svm(f, data = data, kernel = "radial", cost = 10),
-  gbm = function(f,data) gbm(f,data = data, n.trees= 10000,interaction.depth = 1, shrinkage = 0.01),
-  gbm2 = function(f,data) gbm(f,data = data, n.trees= 10000,interaction.depth = 3, shrinkage = 0.01),
-  gbm3 = function(f,data) gbm(f,data = data, n.trees= 20000,interaction.depth = 1, shrinkage = 0.01),
-  gbm4 = function(f,data) gbm(f,data = data, n.trees= 10000,interaction.depth = 1, shrinkage = 0.05),
-  gbm5 = function(f,data) gbm(f,data = data, n.trees= 500,interaction.depth = 12, shrinkage = 0.0001),
-  glm_Gam_log               = function(f, data) glm(f, data = data, family = Gamma(link = "log")),
-  glm_Gam_ident              = function(f, data) glm(f, data = data, family = Gamma(link = "identity")),
-  glm_Gau_log               = function(f, data) glm(f, data = data, family = gaussian(link = "log"))
+  #gbm = function(f,data) gbm(f,data = data, n.trees= 10000,interaction.depth = 1, shrinkage = 0.01),
+  #gbm2 = function(f,data) gbm(f,data = data, n.trees= 10000,interaction.depth = 3, shrinkage = 0.01),
+  #gbm3 = function(f,data) gbm(f,data = data, n.trees= 20000,interaction.depth = 1, shrinkage = 0.01),
+  #gbm4 = function(f,data) gbm(f,data = data, n.trees= 10000,interaction.depth = 1, shrinkage = 0.05),
+  #gbm5 = function(f,data) gbm(f,data = data, n.trees= 500,interaction.depth = 12, shrinkage = 0.0001),
+  #glm_Gam_log               = function(f, data) glm(f, data = data, family = Gamma(link = "log")),
+  #glm_Gam_ident              = function(f, data) glm(f, data = data, family = Gamma(link = "identity")),
+  #glm_Gau_log               = function(f, data) glm(f, data = data, family = gaussian(link = "log"))
 )
 
 model_results <- data.frame()
